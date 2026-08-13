fd0d05e52ae4847d1abc49ee6fd3de5f984a5573
sandhya324 <sdhya3842@gmail.com>
2026-08-13 18:32:56 +0530

    Day-2 rivision done

--- Day-1.js
+++ Day-1.js
@@ -1,10 +1,3 @@
-// 
-
-
-
-
-
-
 let city;
 console.log(city);
 console.log(typeof city);
@@ -12,3 +5,5 @@ console.log(typeof city);
 let student = null;
 console.log(student);
 console.log(typeof student);
+
+// node Day-1.js     to run java file
---
+++ Day-2.js
@@ -0,0 +1,16 @@
+let name = "Sandhya";
+let age = 23;
+let isStudent = true;
+let city = "Dehradun";
+
+
+console.log(`Hello, my name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`I live in ${city}.`);
+console.log(`Student: ${isStudent}`);
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof city);
